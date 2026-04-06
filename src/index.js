5f0bec2010f1aba8ad112d01487064750c8402d5
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-04-05 20:03:56 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,77 @@
+﻿import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { hostname } from "node:os";
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
+import { scramjetPath } from "@mercuryworkshop/scramjet/path";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+
+const publicPath = fileURLToPath(new URL("../public/", import.meta.url));
+
+logging.set_level(logging.NONE);
+Object.assign(wisp.options, {
+  allow_udp_streams: false,
+  hostname_blacklist: [/example\.com/],
+  dns_servers: ["1.1.1.3", "1.0.0.3"]
+});
+
+const app = Fastify({
+  serverFactory: (handler) => {
+    return createServer()
+      .on("request", (req, res) => {
+        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        handler(req, res);
+      })
+      .on("upgrade", (req, socket, head) => {
+        if (req.url.endsWith("/wisp/")) {
+          wisp.routeRequest(req, socket, head);
+          return;
+        }
+        socket.end();
+      });
+  }
+});
+
+app.register(fastifyStatic, {
+  root: publicPath,
+  decorateReply: true
+});
+
+app.register(fastifyStatic, {
+  root: scramjetPath,
+  prefix: "/scram/",
+  decorateReply: false
+});
+
+app.register(fastifyStatic, {
+  root: libcurlPath,
+  prefix: "/libcurl/",
+  decorateReply: false
+});
+
+app.register(fastifyStatic, {
+  root: baremuxPath,
+  prefix: "/baremux/",
+  decorateReply: false
+});
+
+app.setNotFoundHandler((req, reply) => {
+  reply.code(404).type("text/html").sendFile("404.html");
+});
+
+app.server.on("listening", () => {
+  const address = app.server.address();
+  console.log("Listening on:");
+  console.log(`  http://localhost:${address.port}`);
+  console.log(`  http://${hostname()}:${address.port}`);
+});
+
+const port = Number.parseInt(process.env.PORT ?? "8080", 10) || 8080;
+
+app.listen({
+  port,
+  host: "0.0.0.0"
+});
